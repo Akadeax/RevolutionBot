8d08a9b56d277303435992151502225902c8c1c9
Patrick <patrickqwertz@gmail.com>
2020-08-13 20:02:23 +0200

    Initial Commit

---
+++ RedditBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RedditBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
